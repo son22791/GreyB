cbc0168a04cbab919dade1ce78be9279528ce1d3
son22791 <son290396@gmail.com>
2021-06-06 00:16:31 +0700

    them file

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -135,31 +135,31 @@ for (i = 0; i<accordion.length; i++) {
 		this.classList.toggle('minus')
 	})
 }
-$(document).ready(function () {
-	$('#responsive2').lightSlider({
-		item: 4,
-		loop: false,
-		slideMove: 1,
-		easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-		speed: 600,
-		responsive: [{
-				breakpoint: 800,
-				settings: {
-					item: 3,
-					slideMove: 1,
-					slideMargin: 6,
-				}
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					item: 2,
-					slideMove: 1
-				}
-			}
-		]
-	});
-});
+// $(document).ready(function () {
+// 	$('#responsive2').lightSlider({
+// 		item: 4,
+// 		loop: false,
+// 		slideMove: 1,
+// 		easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+// 		speed: 600,
+// 		responsive: [{
+// 				breakpoint: 800,
+// 				settings: {
+// 					item: 3,
+// 					slideMove: 1,
+// 					slideMargin: 6,
+// 				}
+// 			},
+// 			{
+// 				breakpoint: 480,
+// 				settings: {
+// 					item: 2,
+// 					slideMove: 1
+// 				}
+// 			}
+// 		]
+// 	});
+// });
 ///Product detail slider img
 function clickme(smallImg) {
 
